d538f15cd825d65d7b023035006642415caf28e3
Apurva <apurva1996gowda@gmail.com>
2025-12-04 11:38:41 +0530

    first commit of the project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { AppBar, Toolbar } from '@mui/material'
+
+function App() {
+
+  return (
+    <>
+      <AppBar>
+        <Toolbar></Toolbar>
+      </AppBar>
+    </>
+  )
+}
+
+export default App
